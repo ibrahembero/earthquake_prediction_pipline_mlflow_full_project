8f6ff967ef70ff0629ec01d442c0fba2e4343ce2
ibrahembero <ih1004569bero@gmail.com>
2025-02-07 12:29:09 +0300

    mlflow project to track experiments every single run of our pipeline

---
+++ ml_flow_proj.py
@@ -0,0 +1,7 @@
+# we install mlflow lib or tool with: pip install mlflow
+# This will install MLflow, which is an open-source platform for managing the end-to-end
+# machine learning lifecycle, including experimentation, reproducibility, and deployment.
+# with: mlflow ui // we can run the local version of mlflow
+# mlflow server --host 127.0.0.1 --port 5000
+# mlflow server --host 127.0.0.1 --port 5001
+    
