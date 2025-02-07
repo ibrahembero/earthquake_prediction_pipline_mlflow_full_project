dd80cd651ac3f5ed907ee5195fa2ea78e017316d
ibrahembero <ih1004569bero@gmail.com>
2025-02-07 16:04:41 +0300

    script ise to run the env and run the python pipeline

---
+++ pipline_Script.ps1
@@ -0,0 +1,13 @@
+﻿# 1. Set the path to your virtual environment and script
+$venv_path = "C:\bero\shAI\Qafza_Training\Content_Of_Course\week 7\pipeline_earthquake\venv\Scripts\Activate.ps1"
+$script_path = "C:\bero\shAI\Qafza_Training\Content_Of_Course\week 7\pipeline_earthquake\pipeline_earth.py"
+
+# 2. Activate the virtual environment
+Write-Host "Activating virtual environment..."
+. $venv_path  # This activates the virtual environment in PowerShell
+
+# 3. Run the Python script
+Write-Host "Running the Python script..."
+python $script_path
+
+Write-Host "Pipeline execution complete!"
